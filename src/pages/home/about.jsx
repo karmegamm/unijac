93592f711b7177ce64487112de7002c0c68fdaec
karmegamm <karmegamm@anjaconline.org>
2023-04-17 23:26:48 +0530

    new  8

--- src/pages/home/about.jsx
+++ src/pages/home/about.jsx
@@ -3,12 +3,12 @@ import clg from '../../assets/clg.jpg'
 
 function About() {
     return <div className='mb-6 flex flex-col gap-32 md:mt-10 '>
-        <div className='h-full flex flex-col items-start ml-16 md:items-start md:ml-20 md:-mb-32'>
-            <h1 className='text-3xl md:text-5xl font-serif first-letter:text-red-600 py-3'> About Us</h1>
+        <div className='h-full flex flex-col items-start ml-16 md:items-start md:ml-56 md:-mb-32'>
+            <h1 className='text-3xl md:text-5xl font-serif first-letter:text-red-600 py-3 md:translate-x-28 md:translate-y-10'> About Us</h1>
         </div>
         <div className='flex flex-wrap flex-col md:flex-row items-center justify-start md:justify-around -mt-28 '>
             <div className='w-[60%] md:h-96  md:mt-5 flex justify-center md:justify-around md:items-center'>
-                <p className='text-xl w-full md:w-[78%] '>This Unijac technology is a platform full of students. Here,the students of Ayya Nadar Janaki Ammal College are united to work and gaining some knowledge as well as experience.</p>
+                <p className='text-xl w-full md:w-[78%] md:translate-x-28 md:translate-y-11 '>This Unijac technology is a platform full of students. Here,the students of Ayya Nadar Janaki Ammal College are united to work and gaining some knowledge as well as experience.</p>
             </div>
             <img src={clg} className='md:w-[25%] mt-6 w-[65%] mx-auto rounded-2xl mr-14 h-96' alt="" />
         </div>
--- src/pages/home/service.jsx
+++ src/pages/home/service.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import {card} from '../../routes' 
 import { Link } from 'react-router-dom';
 function Service() {
-  return<div className='w-full flex gap-y-9 justify-evenly mt-7 flex-wrap mb-10 '>
+  return<div className='w-full flex gap-y-9 flex-wrap justify-evenly mt-7  mb-10 p-4 '>
         {card.map(({title,src,description,btn_type})=>{
-            return <div className="card  shadow-2xl  hover:bg-gray-500 hover:text-white hover:opacity-100 w-56 md:w-80">
-                <figure><img src={src} alt="Shoes" className='w-44 h-36 md:mt-5 '/></figure>
+            return <div className="card  shadow-2xl  hover:bg-gray-500 hover:text-white hover:opacity-100  md:w-80">
+                <figure><img src={src} alt="Shoes" className='w-40  md:h-36 md:mt-5 '/></figure>
                 <div className="card-body ">
                     <h2 className="text-center text-2xl font-serif font-bold">{title}</h2>
                     <p className='text-center font-thin '>{description}</p>    
