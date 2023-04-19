0343d7621a9e7d72eb423ea2f220d00ebd57730a
karmegamm <karmegamm@anjaconline.org>
2023-04-19 15:54:46 +0530

    new 200

--- src/pages/home/carousels.jsx
+++ src/pages/home/carousels.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { carousels } from '../../routes';
 function Carousel() {
-  return <div className='flex mx-auto h-52 w-[75%] md:w-[68%] md:h-96 md:mt-10 md:mb-10'>
+  return <div className='flex mx-auto h-52 xl:h-96 w-[75%] md:w-[68%] md:h-96 md:mt-10 md:mb-10'>
     <a href="#slide1" className="btn btn-circle md:mt-44 hidden md:flex md:justify-center md:items-center">❮</a> 
     <div className="w-full carousel rounded-box h-full md:mr-5  md:ml-5 ">
     {
