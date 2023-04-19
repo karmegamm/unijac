41d94c1286fb93af1a0c2f76e6f0c51bf13aa414
karmegamm <karmegamm@anjaconline.org>
2023-04-19 15:46:31 +0530

    new 21

--- src/pages/service/section1.jsx
+++ src/pages/service/section1.jsx
@@ -9,7 +9,7 @@ function Section1() {
         {service.map(({img1,head1,para1,img2,head2,para2})=>{
              return <div>
                     <div className='flex justify-center items-center md:flex-row  flex-col hovdiv mb-2'>
-                        <img src={img1} className='z-20 my-2 md:my-0 w-[70%] xl:w-[85%] md:w-[35%] rounded-2xl transition-transform duration-500 hovimg' />
+                        <img src={img1} className='z-20 my-2 md:my-0 w-[70%] xl:w-[75%] md:w-[35%] rounded-2xl transition-transform duration-500 hovimg' />
                         <div className='w-full md:w-[30%]'>
                             <h1 className='text-4xl text-center  p-3 font-serif first-letter:text-red-500 transition-transform duration-300 heading '>{head1}</h1>
                             <p className='p-3  text-gray-600 transition-transform duration-500 goinside'>{para1}</p>
@@ -20,7 +20,7 @@ function Section1() {
                             <h1 className='text-4xl p-3 font-serif text-center first-letter:text-red-500 transition-transform duration-300 heading2 '>{head2}</h1>
                             <p className='p-3  text-gray-600 transition-transform duration-500 goinside2'>{para2}</p>
                         </div>
-                        <img src={img2} className='z-20 w-[70%] xl:w-[85%] md:w-[30%] rounded-2xl transition-transform duration-500 hovimg2' />
+                        <img src={img2} className='z-20 w-[70%] xl:w-[75%] md:w-[30%] rounded-2xl transition-transform duration-500 hovimg2' />
                     </div></div>     
         })}
       </div>
