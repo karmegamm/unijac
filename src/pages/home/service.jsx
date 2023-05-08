0eb7da1c0400058069de7dab0db860c263e3a442
karmegamm <karmegamm@anjaconline.org>
2023-05-08 11:01:30 +0530

    123446

--- src/pages/home/service.jsx
+++ src/pages/home/service.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Service() {
   return<div className='w-full flex xl:gap-y-16 gap-y-9 flex-wrap justify-evenly mt-7  mb-10 p-4 '>
         {card.map(({title,src,description,btn_type})=>{
-            return <div className="card  shadow-2xl  hover:bg-gray-500 hover:text-white hover:opacity-100  md:w-80">
+            return <div className="card  shadow-2xl pt-6 hover:bg-gray-500 hover:text-white hover:opacity-100  md:w-80">
                 <figure><img src={src} alt="Shoes" className='w-40  md:h-36 md:mt-5 '/></figure>
                 <div className="card-body ">
                     <h2 className="text-center text-2xl font-serif font-bold">{title}</h2>
